6f2f67ae1b777e7721dda4a0e13317dbd05e3bf1
Kapena Kaaihue <142773521+Kapena24@users.noreply.github.com>
2023-09-27 00:16:35 -1000

    Completed 1-11

--- HackWichFour/ViewController.swift
+++ HackWichFour/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var favoriteFoodsLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showFavoriteFoods(_ sender: UIButton) {
+    
+    favoriteFoodsLabel.text = "Musahbi, Hawaiian food, Chocolate"
+    }
+    
 }
 
